9d9a137d61a201a56bd04c7fa45102f4a2c829cf
intkilow <intkilow@163.com>
2021-04-20 14:16:34 +0800

    1、readme.me

--- app/src/main/java/top/intkilow/jetpackbase/MainActivity.kt
+++ app/src/main/java/top/intkilow/jetpackbase/MainActivity.kt
@@ -1,29 +1,34 @@
 package top.intkilow.jetpackbase
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.contains
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import top.intkilow.architecture.network.NetWorkManager
 import top.intkilow.jetpackbase.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         NetWorkManager.getInstance().init(HashMap(), "https://www.baidu.com/api/")
+     /*   @Suppress("DEPRECATION")
+        run {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.WHITE
+        }*/
+
 
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-/*
-        val FLAGS_FULLSCREEN =
-            View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-
-        binding.navHost.systemUiVisibility = FLAGS_FULLSCREEN*/
-//        ActivityMainBinding.
     }
 }
